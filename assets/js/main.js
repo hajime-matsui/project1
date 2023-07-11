1dfdcfe5f24a236a140e6702eadd508a9617d38f
hajime-matsui <130442159+hajime-matsui@users.noreply.github.com>
2023-07-11 17:29:13 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ function updateTable(tableId, type) {
 
     // loop over table rows
     for(let i=1; i < el.rows.length; i++){
-        let nextTotal;
+        let nextTotal = 0;
         const currentTotal = parseInt(el.rows[i].cells[2].innerHTML);
         
         if(type === 'multiply'){
