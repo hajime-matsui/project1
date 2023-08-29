7c3e30f0935cb0a5676685aeb497b9a6e40ef0d0
hajime-matsui <130442159+hajime-matsui@users.noreply.github.com>
2023-08-29 16:28:53 +0900

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,18 @@
+const loginbutton = document.getElementById("login-form-submit");
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const loginForm = document.getElementById("login-form");
+  const loginErrorMsgHolder = document.getElementById("login-error-msg-holder");
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
 
+  if (username === "user" && password === "password") {
+    alert("You have successfully logged in.");
+    location.reload();
+  } else {
+    alert("Invalid username or password!");
+    loginErrorMsgHolder.style.display = "block";
+  }
+})
+  
+   
